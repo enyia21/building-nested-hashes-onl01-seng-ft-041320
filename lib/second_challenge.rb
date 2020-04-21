eb90d74b1661f6122dd757ec197d7a84f9837d1e
Chibuisi Enyia <enyia21@gmail.com>
2020-04-21 01:44:01 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -5,7 +5,7 @@ def second_challenge
       :matriarch => {},
       :hero => {},
       :hero_friends => {}
-      }, 
+      },
     :capulet => {
       #your key/value pairs here!
       :patriarch => {},
@@ -14,5 +14,5 @@ def second_challenge
       :heroine_friends => {}
       }}
 
-  
+
 end
